6345d27363c53e2b74df2ad0aa925c65beac3a2f
Matthias Wahl <matthiaswahl@m7w3.de>
2018-01-18 22:16:01 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+
+setup(
+    name="ponylang-mkdocs-theme",
+    version=VERSION,
+    url='http://www.mkdocs.org',
+    license='BSD',
+    description='Ponylang theme for MkDocs',
+    author='Matthias Wahl',
+    author_email='matthiaswahl@m7w3.de',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'bootstrap = mkdocs_ponylang',
+        ]
+    },
+    zip_safe=False
+)
